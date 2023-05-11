f95fba74d2609c07c2817b16b66577db055f30f4
Zanna Fitch <zannafitch2004@gmail.com>
2023-05-11 13:20:13 -0500

    add deck and round information

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,30 @@
+
+
 class Deck
-    
+attr_reader :cards
+    def initialize(cards)
+        @cards = cards
+        
+    end
+
+
+
+
+
+
+
+    def cards_in_category(category)
+    # p  @card.select {|card, category| card.select(category)}
+
+    cards.select {|card| card.category == category}
+
+
+    end
+
+    def count
+    p cards.length
+    end
+
 end
+
+# rspec spec/deck_spec.rb
---
+++ lib/round.rb
@@ -0,0 +1,26 @@
+class Round
+    attr_reader :deck, :turns
+        def initialize(deck)
+            @deck = deck
+            @turns = []
+            
+        end
+
+
+
+
+
+def take_turn
+
+end
+
+def current_card
+    p deck.shift
+
+# .first
+# .shift
+# .take(n)
+
+end
+
+end
